0401b779f0628e57830e280222f40ab541bea381
Nemanja <teranjecirkusa@gmail.com>
2019-06-24 10:39:54 +0200

    Komitovani fajlovi sa click on button

--- kvizapp.js
+++ kvizapp.js
@@ -1,6 +1,56 @@
+//DOM varibales 
+
+var firstButton = document.getElementById("prvo");
+var secondButton = document.getElementById("drugo");
+var thirdButton = document.getElementById("trece");
+var fourthButton = document.getElementById("cetvrto");
 
 
 
+
+
+
+
+
+
+//Functions
+
+function disablebutton() {
+firstButton.disabled = true;
+secondButton.disabled = true; 
+thirdButton.disabled = true; 
+fourthButton.disabled = true;
+
+}
+
+
+
+firstButton.onclick = function () {
+this.style.backgroundColor = "red";
+this.innerHTML = "Wrong!";
+disablebutton();
+
+
+};
+
+secondButton.onclick = function () {
+this.style.backgroundColor = "red";
+this.innerHTML = "Wrong!";
+disablebutton();
+
+};
+
+thirdButton.onclick = function () {
+this.style.backgroundColor = "green";
+this.innerHTML = "Correct";
+disablebutton();
+};
+
+fourthButton.onclick = function () {
+this.style.backgroundColor = "red";
+this.innerHTML = "Wrong!";
+disablebutton();
+};
 
 
 
