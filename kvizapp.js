48ef39f443f9b539ffd2ae65074a88667676f79d
Nemanja <teranjecirkusa@gmail.com>
2019-06-27 11:53:18 +0200

    Komitovan i drugo pitanje za kviz

--- kvizapp.js
+++ kvizapp.js
@@ -1,26 +1,41 @@
 //DOM varibales 
 
+//HISTORY QUESTION 1
+
 var firstButton = document.getElementById("prvo");
 var secondButton = document.getElementById("drugo");
 var thirdButton = document.getElementById("trece");
 var fourthButton = document.getElementById("cetvrto");
 
+//HISTROY QUESTION 2 
 
-//Functions
+var fButton = document.getElementById("first");
+var sButton = document.getElementById("second");
+var tButton = document.getElementById("third");
+var foButton = document.getElementById("fourth");
+
+
+//Function Disable 
 
 function disablebutton() {
 firstButton.disabled = true;
 secondButton.disabled = true; 
 thirdButton.disabled = true; 
 fourthButton.disabled = true;
+fButton.disabled = true;
+sButton.disabled = true; 
+tButton.disabled = true; 
+foButton.disabled = true;
 
 }
+
+//Function Refresh
 
 function refreshPage(){
     window.location.reload();
 } 
 
-//BUTTON FUNCTION 
+//BUTTON FUNCTION HISTORY 1  
 
 firstButton.onclick = function () {
 this.style.backgroundColor = "red";
@@ -37,7 +52,7 @@ disablebutton();
 
 thirdButton.onclick = function () {
 this.style.backgroundColor = "green";
-this.innerHTML = "Correct";
+this.innerHTML = "Correct!";
 disablebutton();
 };
 
@@ -47,5 +62,30 @@ this.innerHTML = "Wrong!";
 disablebutton();
 };
 
+//BUTTON FUNCTION HISTORY 2
 
+fButton.onclick = function () {
+this.style.backgroundColor = "red";
+this.innerHTML = "Wrong!";
+disablebutton();
+};
+    
+sButton.onclick = function () {
+this.style.backgroundColor = "red";
+this.innerHTML = "Wrong!";
+disablebutton();
+    
+};
+    
+tButton.onclick = function () {
+this.style.backgroundColor = "red";
+this.innerHTML = "Wrong!";
+disablebutton();
+};
+    
+foButton.onclick = function () {
+this.style.backgroundColor = "green";
+this.innerHTML = "Correct!";
+disablebutton();
+};
 
