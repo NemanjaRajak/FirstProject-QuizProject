dd24fb403367d3e1d491afc34b81835f5f8e5104
Nemanja <teranjecirkusa@gmail.com>
2019-07-29 08:22:34 +0200

    Zavrsen sajt

--- kvizapp.js
+++ kvizapp.js
@@ -21,7 +21,12 @@ var numero2Button = document.getElementById("numero2");
 var numero3Button = document.getElementById("numero3");
 var numero4Button = document.getElementById("numero4");
 
+//OSTALO QUESTION 1
 
+var number1Button = document.getElementById("number1");
+var number2Button = document.getElementById("number2");
+var number3Button = document.getElementById("number3");
+var number4Button = document.getElementById("number4");
 
 
 
@@ -40,6 +45,10 @@ numero1Button.disabled = true;
 numero2Button.disabled = true;
 numero3Button.disabled = true;
 numero4Button.disabled = true;
+number1Button.disabled = true;
+number2Button.disabled = true;
+number3Button.disabled = true;
+number4Button.disabled = true;
 
 }
 
@@ -139,6 +148,40 @@ numero4Button.onclick = function() {
 
     this.style.backgroundColor = "red";
     this.innerHTML = "Wrong!"; 
+    disablebutton();
+
+};
+
+//BUTTON FUNCTION OSTALO 1
+
+number1Button.onclick = function() {
+
+    this.style.backgroundColor = "red";
+    this.innerHTML = "Wrong!"; 
+    disablebutton();
+
+};
+
+number2Button.onclick = function() {
+
+    this.style.backgroundColor = "red";
+    this.innerHTML = "Wrong!"; 
+    disablebutton();
+
+};
+
+number3Button.onclick = function() {
+
+    this.style.backgroundColor = "red";
+    this.innerHTML = "Wrong!"; 
+    disablebutton();
+
+};
+
+number4Button.onclick = function() {
+
+    this.style.backgroundColor = "green";
+    this.innerHTML = "Correct!"; 
     disablebutton();
 
 };
